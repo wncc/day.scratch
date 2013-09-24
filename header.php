b406200a61a94f1de5ab2b436f902b399cad54ed
Sushant <hiraysushant@gmail.com>
2013-09-24 18:42:57 +0530

    Removing common elements into a single zone

---
+++ header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!-- saved from url=(0031)http://day.scratch.mit.edu/home -->
+<html>
+  <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+      <title>Scratch Day</title>
+      
+      <script src="./scratch_files/maps" type="text/javascript"></script><script src="./scratch_files/main.js" type="text/javascript"></script><style type="text/css">@media print{.gmnoprint{display:none}}@media screen{.gmnoscreen{display:none}}</style>
+      <link rel="shortcut icon" href="http://day.scratch.mit.edu/sites/default/files/pictures/favicon.png" type="image/x-icon">
+        <link type="text/css" rel="stylesheet" media="all" href="./scratch_files/3dbce13e3e0104029952f4239c8957e0.css">
+      <link type="text/css" rel="stylesheet" media="all" href="./scratch_files/css_1c568289f6252198d77ba0ab1802e6f3.css">
+      
+      <link type="text/css" rel="stylesheet" media="all" href="./scratch_files/ui.theme.css">
+      
+      <!--[if lt IE 7]>
+        <link rel="stylesheet" href="/sites/all/themes/ad_blueprint/ie_lt7.css" type="text/css">
+      <![endif]-->
+      <script type="text/javascript" src="./scratch_files/js_d031147d2e20a652d59aa957bb98decf.js"></script>
+      <script type="text/javascript" src="./scratch_files/gmap_markers.js"></script>
+      <script type="text/javascript"> </script>
+      <script type="text/javascript" charset="UTF-8" src="./Scratch   Day_files/{mod_drag,mod_ctrapi,mod_zoom,mod_scrwh}.js"></script>
+  </head>
+  <body class="none">
+    <div id="wrapper1">
+      <div id="wrapper2">
+        <div id="wrapper3">
+          <div id="wrapper4" class="navbar-1">
+            <div class="clear-block">
+              <div id="header" class="clear-block">
+                <div>
+                  <div id="logo-sitename">
+                    <div class="clear-block">
+                        <a href="http://day.scratch.wncc-iitb.org/" title="Home" id="logo">
+                          <img src="./scratch_files/ad_blueprint_logo.png" alt="Home">
+                        </a> 
+                        <a href="http://day.scratch.wncc-iitb.org/" title="Home" id="logo">
+                          <img style="padding-left:150px;padding-right:0px;padding-top:100px" width="200px" src="./scratch_files/wncc_logo.png" alt="Home">
+                        </a>    
+                    </div>
+                  </div>
+                </div>
+              </div>  
+              <div id="right-header">
+                <div id="block-block-3" class="block block-block">
+                  <div class="content">
+                    <a href="/register">Register Here!</a>  
+                  </div>
+                </div>
+              </div>
+            </div>
+          
+            <div id="middle" class="clear-block">
+              <div id="main-outer">
+              <div id="main-outer2">
+                <div>
+                  <div id="main">
+                    <div id="main2">
+                      <div id="main-inner">
+                        <div id="main-inner2">
+                          <div class="inner">
+                            <div class="panel-flexible panels-flexible-1 clear-block">
+                              <div class="panel-flexible-inside panels-flexible-1-inside">
+                                <div class="panels-flexible-row panels-flexible-row-1-row_2 panels-flexible-row-last clear-block">
+                                  <div class="inside panels-flexible-row-inside panels-flexible-row-1-row_2-inside panels-flexible-row-inside-last clear-block">
+                                    <div class="panels-flexible-region panels-flexible-region-1-row_2_1 panels-flexible-region-first ">
+                                      <div class="inside panels-flexible-region-inside panels-flexible-region-1-row_2_1-inside panels-flexible-region-inside-first">
+                                        <div class="panel-pane pane-custom pane-1">
+                                          <div class="pane-content">
---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+include('header.php')
+
+       <h1>Attend Scratch Day</h1><br>
+                                            <div>Scratch Day is a worldwide network of gatherings, where people come together to meet other Scratchers, share projects and experiences, and learn more about 
+                                              <a href="http://scratch.mit.edu/" target="_blank">Scratch</a>
+                                            </div>
+                                            <br>
+                                            <div>This the first Scratch Day organized in IIT Bombay!
+                                            </div>
+                                            <br>
+                                            <p>Here's the <a href="http://www.youtube.com/watch?v=wjVmUDW89zQ" target="_blank">Scratch Day 2013 welcome video</a> from members of the MIT Scratch Team to you and your Scratch Day participants. 
+                                            </p>
+                                            
+                                           
+                                            <p> <a href="/register">Register</a> for scratch day with teams of 2! Create your imagination and win exciting <a href="/prizes">prizes</a> :) </p>
+                                          </div>
+                                        </div>
+                                        <div class="pane-content">
+                                            <h1>Agenda</h1>
+                                            Well since most of you don't know what Sratch is, we will start with introductory sessions on Scratch.
+                                            The introductory sessions are held in your hostels for your convenience, the details are as follows:
+                                            <ul>
+												<li>25 September, Hostel 15 Mess, Post Dinner (9 pm Onwards)</li>
+												<li>26 September, Hostel 4 Mess, Post Dinner (9 pm Onwards)</li>
+													
+											</ul>
+											The final scratch days are held on September 28 and September 29 in New Software Lab (NSL) in Old CSE Department.
+											 
+											
+                                        </div>
+                                        
+                                      </div>
+                                    </div>
+                                    <div class="panels-flexible-region panels-flexible-region-1-row_2_2">
+                                      <div class="inside panels-flexible-region-inside panels-flexible-region-1-row_2_2-inside">
+                                      </div>
+                                    </div>
+                                    <div class="panels-flexible-region panels-flexible-region-1-row_2_3 panels-flexible-region-last ">
+                                      <div class="inside panels-flexible-region-inside panels-flexible-region-1-row_2_3-inside panels-flexible-region-inside-last">
+                                        <div class="panel-pane pane-custom pane-2">
+                                          <div class="pane-content">
+                                            <h1>What is Sratch?</h1>
+                                            <br>
+                                            <div><iframe src="http://player.vimeo.com/video/65583694?title=0&amp;byline=0&amp;portrait=0&amp;color=ff9933" width="370" height="231" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> 
+                                            </div>
+                                            <div>
+                                            Scratch is a programming language!
+                                            </div>
+                                            <div>
+
+With Scratch, you can program your own interactive stories, games, and animations — and share your creations with others in the online community. 
+                                            </div>
+                                            <br>
+                                            
+include('footer.php')
+
+
+
+?>
