400e4d89daedc6d73bc04d55711d5a8a1d9f7525
Sushant <hiraysushant@gmail.com>
2013-09-24 18:50:51 +0530

    Added Team Details

---
+++ about.php
@@ -0,0 +1,29 @@
+<?php
+
+include('header.php')
+
+<h1>Team WnCC</h1><br>
+<div>The awesome Scratch Day is conducted by the Web and Coding Club of IIT B. The team comprises of the following members:
+                                              <ul>
+                                              <li>Sushant Hiray <i>Manager</i></li>
+                                              <li>Guna Prasad <i>Manager</i></li>
+                                              <li>Aditya Kumar Akash <i>Convener</i></li>
+                                              <li>Prateek Chandan <i>Convener</i></li>
+                                              <li>Pranav Tiwari <i>Convener</i></li>
+                                              <li>Ranveer Agarwal <i>Convener</i></li>
+                                              <li>Rohit Kumar <i>Convener</i></li>
+                                              <li>Sai Priyatham <i>Convener</i></li>
+                                              <li>Syamantak Naskar <i>Convener</i></li>
+                                              <li>Manish Goregaokar <i>Convener</i></li>
+                                              </ul>
+</div>
+<br>
+<div>Send your love by joining the WnCC <a target="_blank" href="https://groups.google.com/forum/#!forum/wncc_iitb" >Google Group</a> and following our <a href="http://www.facebook.com/wncc.iitb">Facebook Page</a>
+</div>
+<br>
+
+
+include('footer.php')
+
+
+?>
