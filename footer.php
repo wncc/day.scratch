6c55838d0c5ba3b817f019e9c82f6e88abfad574
WnCC <wncc.itsp@gmail.com>
2013-09-26 17:57:29 +0000

    Adding necessary files

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
-<iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fwncc.iitb%2Flike&amp;width=450&amp;height=80&amp;colorscheme=light&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;send=true&amp;appId=215790075234188" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe>
+<?php
+echo "<iframe src='http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fwncc.iitb%2Flike&amp;width=450&amp;height=80&amp;colorscheme=light&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;send=true&amp;appId=215790075234188' scrolling='no' frameborder='0' style='border:none; overflow:hidden; width:450px; height:80px;' allowTransparency='true'></iframe>
                                             <br>
                                           </div>
                                         </div>
@@ -16,15 +17,15 @@
                   </div>
                 </div>
 
-                <div id="navbar">
-                  <div id="primary">
-                    <ul class="links">
-                      <li class="menu-153 active-trail first active"><a href="/home" title="" class="active">Home</a></li>
-                      <li class="menu-886"><a href="/register" title="Media">Registrations</a></li>
-                      <li class="menu-886"><a href="/prizes" title="Media">Prizes</a></li>
-                      <li class="menu-133"><a href="/resources" title="">Resources</a></li>                      
-                      <li class="menu-132"><a href="/store" title="">Store</a></li>
-                      <li class="menu-503 last"><a href="/about" title="">About</a></li>
+                <div id='navbar'>
+                  <div id='primary'>
+                    <ul class='links'>
+                      <li class='menu-153 active-trail first active'><a href='/home' title='' class='active'>Home</a></li>
+                      <li class='menu-886'><a href='/register' title='Media'>Registrations</a></li>
+                      <li class='menu-886'><a href='/prizes' title='Media'>Prizes</a></li>
+                      <li class='menu-133'><a href='/resources' title=''>Resources</a></li>                      
+                      <li class='menu-132'><a href='/store' title=''>Store</a></li>
+                      <li class='menu-503 last'><a href='/about' title=''>About</a></li>
                     </ul>          
                   </div>
                 </div>      
@@ -32,27 +33,27 @@
             </div>
           </div>
         </div>
-        <div id="footer">
-          <div id="footer-inner">
-            <div id="block-block-4" class="block block-block">
-              <div class="content">
-                <div id="logo-sitename">
-                    <div style="padding-left:50px" class="clear-block">
-                        <a href="http://day.scratch.mit.edu/" title="Home" id="logo">
-                          <img style="padding-left:50px;padding-right:50px;padding-top:20px" width="50px" src="./scratch_files/iitb_logo.png" alt="Home">
+        <div id='footer'>
+          <div id='footer-inner'>
+            <div id='block-block-4' class='block block-block'>
+              <div class='content'>
+                <div id='logo-sitename'>
+                    <div style='padding-left:50px' class='clear-block'>
+                        <a href='http://day.scratch.mit.edu/' title='Home' id='logo'>
+                          <img style='padding-left:50px;padding-right:50px;padding-top:20px' width='50px' src='./scratch_files/iitb_logo.png' alt='Home'>
                         </a>   
-                        <a href="http://day.scratch.mit.edu/" title="Home" id="logo">
-                          <img style="padding-left:50px;padding-right:50px;padding-top:20px" width="100px" src="./scratch_files/wncc_logo.png" alt="Home">
+                        <a href='http://day.scratch.mit.edu/' title='Home' id='logo'>
+                          <img style='padding-left:50px;padding-right:50px;padding-top:20px' width='100px' src='./scratch_files/wncc_logo.png' alt='Home'>
                         </a>   
-                        <a href="http://day.scratch.mit.edu/" title="Home" id="logo">
-                          <img style="padding-left:50px;padding-right:50px;padding-top:20px" width="130px" src="./scratch_files/stab_logo.png" alt="Home">
+                        <a href='http://day.scratch.mit.edu/' title='Home' id='logo'>
+                          <img style='padding-left:50px;padding-right:50px;padding-top:20px' width='130px' src='./scratch_files/stab_logo.png' alt='Home'>
                         </a>   
-                        <a href="http://day.scratch.mit.edu/" title="Home" id="logo">
-                          <img style="padding-left:50px;padding-right:50px;padding-top:0px" width="100px" src="./scratch_files/scratch_logo.png" alt="Home">
+                        <a href='http://day.scratch.mit.edu/' title='Home' id='logo'>
+                          <img style='padding-left:50px;padding-right:50px;padding-top:0px' width='100px' src='./scratch_files/scratch_logo.png' alt='Home'>
                         </a>  
 
                     </div>
-                    <div class="clear-block">
+                    <div class='clear-block'>
                         
                     </div>
                   </div>
@@ -64,5 +65,5 @@
       </div>
     </div>
   </body>
-</html>
-
+</html>";
+?>
