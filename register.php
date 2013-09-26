d62d9d5e74cad286263c3ddbfaf89af9881cd34c
gunaprsd <gunaprsd@gmail.com>
2013-09-26 23:37:55 +0530

    updating register.php

--- register.php
+++ register.php
@@ -1,7 +1,16 @@
 <?php
 include 'header.php';
 <form>
-
+<legend>Register your team!</legend>
+<table>
+<tr><td>Member1 Name : </td><td><input type='text' name='name1'></td></tr>
+<tr><td>Member1 Phone : </td><td><input type='text' name='phone1'></td></tr>
+<tr><td>Member1 E-mail : </td><td><input type='text' name='mail1'></td></tr>
+<tr><td>Member2 Name : </td><td><input type='text' name='name2'></td></tr>
+<tr><td>Member2 Phone : </td><td><input type='text' name='phone2'></td></tr>
+<tr><td>Member2 E-mail : </td><td><input type='text' name='mail2'></td></tr>
+</table>
+<input type='submit' name='submit' value='Register'>
 </form>
 include 'footer.php';
 ?>
