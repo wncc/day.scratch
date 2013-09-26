3d7a5047d9af34be3faee3688ae564c517b4a7c8
WnCC <wncc.itsp@gmail.com>
2013-09-26 18:13:35 +0000

    updating register.php

--- register.php
+++ register.php
@@ -1,14 +1,14 @@
 <?php
 include 'header.php';?>
-<form>
+<form method='post' action='reg.php'>
 <legend>Register your team!</legend>
 <table>
-<tr><td>Member1 Name : </td><td><input type='text' name='name1'></td></tr>
-<tr><td>Member1 Phone : </td><td><input type='text' name='phone1'></td></tr>
-<tr><td>Member1 E-mail : </td><td><input type='text' name='mail1'></td></tr>
-<tr><td>Member2 Name : </td><td><input type='text' name='name2'></td></tr>
-<tr><td>Member2 Phone : </td><td><input type='text' name='phone2'></td></tr>
-<tr><td>Member2 E-mail : </td><td><input type='text' name='mail2'></td></tr>
+<tr><td>Member1 Name : </td><td><input width='100px' type='text' name='name1'></td></tr>
+<tr><td>Member1 Phone : </td><td><input width='100px' type='text' name='phone1'></td></tr>
+<tr><td>Member1 E-mail : </td><td><input width='100px' type='text' name='mail1'></td></tr>
+<tr><td>Member2 Name : </td><td><input width='100px' type='text' name='name2'></td></tr>
+<tr><td>Member2 Phone : </td><td><input width ='100px' type='text' name='phone2'></td></tr>
+<tr><td>Member2 E-mail : </td><td><input width = '100px' type='text' name='mail2'></td></tr>
 </table>
 <input type='submit' name='submit' value='Register'>
 </form>
